9d3245b701cf377b270ad8f31bb6428523083220
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-04-18 15:55:20 +0000

    Minor changes to app

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,25 +1,34 @@
-import React, {useState} from 'react';
-import 'components/InterviewerList.scss';
-import InterviewerListItem from './InterviewerListItem';
+import React from "react";
+import PropTypes from "prop-types";
+import "components/InterviewerList.scss";
+import InterviewerListItem from "./InterviewerListItem";
+
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
 
 export default function InterviewerList(props) {
-  const {interviewers, onChange, value} = props;
+  const { interviewers, onChange, value } = props;
 
-  const interviewerList = interviewers.map(interview=> 
-    <InterviewerListItem 
-      key = {interview.id}
-      id= {interview.id}
-      name = {interview.name}
-      avatar = {interview.avatar}
-      selected = {interview.id === value}
-      setInterviewer = {()=> onChange(interview.id)}
+  
+
+  const interviewerList = interviewers.map((interview) => (
+    <InterviewerListItem
+      key={interview.id}
+      id={interview.id}
+      name={interview.name}
+      avatar={interview.avatar}
+      selected={interview.id === value}
+      setInterviewer={() => onChange(interview.id)}
     />
-  )
+  ));
 
-  return(
-<section className="interviewers">
-  <h4 className="interviewers__header text--light">Interviewer</h4>
-  <ul className="interviewers__list"> {interviewerList}</ul>
-</section>
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list"> {interviewerList}</ul>
+    </section>
   );
 }
+
--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -16,7 +16,7 @@ function useApplicationData() {
   useEffect(() => {
     Promise.all([
       axios.get("api/days"),
-      axios.get("/api/appointments"),
+      axios.get("api/appointments"),
       axios.get("api/interviewers"),
     ])
       .then((response) => {
